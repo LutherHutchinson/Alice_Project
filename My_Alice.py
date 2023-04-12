a889ed22714cbd2e158f7cf0ece1656a6726d470
LutherHutchinson <mariabychkova77@gmail.com>
2023-04-12 15:49:40 +0500

    Sort phrase: Bot phrase and User phrase

--- My_Alice.py
+++ My_Alice.py
@@ -3,13 +3,15 @@ from random import choice
 
 from flask import Flask, request, jsonify
 
-from Key_Phrase import *
+from User_Key_Phrase import *
+from Bot_Key_Phrase import *
 
 app = Flask(__name__)
 
 logging.basicConfig(level=logging.DEBUG)
 
 Episode = 'Начало'
+count = 3
 
 
 @app.route("/", methods=["POST"])
@@ -34,7 +36,7 @@ def handle_dialog(response, request):
     elif Episode == 'Выбор начать игру или нет':
         choose(response)
     else:
-        end(response)
+        pass
 
 
 def start(response):
@@ -55,8 +57,7 @@ def choose(response):
     for disagree in Rejection:
         if disagree in request.json["request"]["command"]:
             return end(response)
-    count = 3
-    return choose_mistake(response, count)
+    return choose_mistake(response)
 
 
 def end(response):
@@ -69,18 +70,20 @@ def mistake(response):
     return
 
 
-def choose_mistake(response, count):
+def choose_mistake(response):
+    global count
     if count == 3:
         response["response"]["text"] = choice(IncorrectStep1)
         for agree in Agreement:
             if agree in request.json["request"]["command"]:
                 response["response"][
                     "text"] = f'{choice(Continue)} {choice(Choose)}'
-            return
+                return
         for disagree in Rejection:
             if disagree in request.json["request"]["command"]:
                 return end(response)
-        return choose_mistake(response, count - 1)
+        count -= 1
+        return choose_mistake(response)
     elif count == 2:
         response["response"]["text"] = choice(IncorrectStep2)
         for agree in Agreement:
@@ -92,7 +95,7 @@ def choose_mistake(response, count):
             if disagree in request.json["request"]["command"]:
                 return end(response)
         count -= 1
-        return choose_mistake(response, count)
+        return choose_mistake(response)
     else:
         response["response"]["text"] = IncorrectStep3
         return end(response)
